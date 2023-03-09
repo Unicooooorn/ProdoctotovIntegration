d87ba7e0de59dc99e2343679868cdb0c43167743
ivanov_iv <ivanov_iv@medinfocenter.ru>
2023-03-09 18:43:22 +0300

    Add infrastructure

---
+++ ProdoctorovIntegration.Infrastructure/Configuration/ServiceProviderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ProdoctorovIntegration.Infrastructure.Configuration
+{
+    public static class ServiceProviderExtensions
+    {
+        public static void ApplyPendingMigrations(this IApplicationBuilder app)
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+            var dbContext = scope.ServiceProvider.GetService<>()!;
+            var pendingMigrations = dbContext.Database.GetPendingMigrations();
+            if (pendingMigrations.Any())
+                dbContext.Database.Migrate();
+        }
+    }
+}
--- ProdoctotovIntegration.Api/Program.cs
+++ ProdoctotovIntegration.Api/Program.cs
@@ -1,3 +1,4 @@
+using ProdoctorovIntegration.Infrastructure;
 using Serilog;
 
 var logger = new LoggerConfiguration().CreateLogger();
@@ -6,3 +7,26 @@ Log.Logger = logger;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseSerilog(logger);
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+if(app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+
+app.ApplyPendingMigrations();
+
+app.UsePathBase(builder.Configuration["PathBase"]);
+
+app.UseRouting();
+app.MapControllers();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+await app.RunAsync();
