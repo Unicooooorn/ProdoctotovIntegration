c0ec9d3543c0d27e812d8ea0a5160dd36e58eabc
Unicooooorn <mr.chikotilo@gmail.com>
2023-03-16 20:26:28 +0300

    add job tests

---
+++ ProdoctorovIntegration.Tests/Job/SendScheduleJobTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using ProdoctorovIntegration.Application.Mapping;
+using ProdoctorovIntegration.Application.Requests.Schedule;
+using ProdoctorovIntegration.Application.Services;
+using ProdoctorovIntegration.Infrastructure.Jobs;
+using ProdoctorovIntegration.Tests.Common.Fakers;
+using Quartz;
+using Xunit;
+using Xunit.Sdk;
+using Exception = System.Exception;
+
+namespace ProdoctorovIntegration.Tests.Job;
+
+public class SendScheduleJobTests
+{
+    private readonly Mock<ISendScheduleService> _sendScheduleService;
+    private readonly Mock<IScopedRequestExecutor> _scopedRequestExecutor;
+
+    public SendScheduleJobTests()
+    {
+        _sendScheduleService = new Mock<ISendScheduleService>();
+        _scopedRequestExecutor = new Mock<IScopedRequestExecutor>();
+    }
+
+    private SendScheduleJob Sut()
+    {
+        return new SendScheduleJob(_sendScheduleService.Object, new NullLogger<SendScheduleJob>(),
+            _scopedRequestExecutor.Object);
+    }
+
+    [Fact]
+    public async Task SendAllEvents()
+    {
+        //Arrange
+        var cell = new EventFaker().Generate();
+        var cells = new[] { cell };
+        _scopedRequestExecutor.Setup(x => x.Execute(It.IsAny<GetScheduleRequest>()))
+            .ReturnsAsync(cells.MapToResponse().ToList().AsReadOnly);
+        //Act
+        await Sut().Execute(Mock.Of<IJobExecutionContext>());
+        //Assert
+        _sendScheduleService.Verify(
+            x => x.SendScheduleAsync(It.IsAny<IReadOnlyCollection<GetScheduleResponse>>(),
+                It.IsAny<CancellationToken>()));
+    }
+}
